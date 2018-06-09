ef2e50665df9d1818df42cad3198294cc35d125a
pramesywaraj <pramesywara_jp@apps.ipb.ac.id>
2018-06-10 00:19:05 +0700

    First Commit: Trying NodeJS

---
+++ api/routes/users.js
@@ -0,0 +1,115 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
+
+//-----------Sign Up-----------//
+
+router.post('/signup', (req, res, next) => {
+    User.find({email: req.body.email})
+        .exec()
+        .then(user => {
+            if (user.length >= 1) {
+                return res.status(409).json({
+                    message: 'Mail exists'
+                });
+            } else {
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    if(err) {
+                        return res.status(500).json({
+                            error: err    
+                        });
+                    } else {
+                        const user = new User({
+                            _id: new mongoose.Types.ObjectId(),
+                            email: req.body.email,
+                            password: hash,
+                            name: req.body.name,
+                            address: req.body.address,
+                            phone_number: req.body.phone_number
+                        });
+                        user.save()
+                            .then(result => {
+                                console.log(result);
+                                res.status(201).json({
+                                    message: 'User created'
+                                });
+                            })
+                            .catch(err => {
+                                console.log(err);
+                                res.status(500).json({
+                                    error: err
+                                });
+                            });
+                    }
+                })
+            }
+        });
+});
+
+//-----------Sign In-----------//
+router.post("/login", (req, res, next) => {
+    User.find({ email: req.body.email })
+        .exec()
+        .then(user => {
+            if(user.length < 1) {
+                return res.status(401).json({
+                    message: 'Auth failed'
+                });
+            }
+            bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+                if(err) {
+                    return res.status(401).json({
+                        message: 'Auth failed'
+                    });
+                }
+                if(result) {
+                    const token = jwt.sign({
+                        email: user[0].email,
+                        userId: user[0]._id
+                    }, 
+                    "bismillah", 
+                    {
+                        expiresIn: "1h"    
+                    },
+                );
+                    return res.status(200).json({
+                        message: 'Auth successful',
+                        token: token
+                    });
+                }
+                res.status(401).json({
+                    message: 'Auth failed'
+                });
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+});
+
+//-----------USERS CRUD-----------//
+
+router.delete('/:userId', (req, res, next) => {
+    User.remove({ _id: req.params.userId })
+        .exec()
+        .then(res => {
+            res.status(200).json({
+                message: "User deleted"
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+});
+
+module.exports = router;
