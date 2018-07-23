7138fee3bbfe61faac8b637733bcbdf65e31a472
firgiawansaktyo <firgiawansaktyo@gmail.com>
2018-07-23 08:48:00 +0700

    failmulu

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,7 @@
 module.exports = {
         mongodb: {
-            // dbURI: 'mongodb://eventarich:eventarich11@ds131711.mlab.com:31711/eventarich_me'
-            dbURI: 'mongodb://admineventarich:eventarich123@ds131711.mlab.com:31711/eventarich_me'
+            dbURI: 'mongodb://eventarich:eventarich11@ds131711.mlab.com:31711/eventarich_me'
+            // dbURI: 'mongodb://admineventarich:eventarich123@ds131711.mlab.com:31711/eventarich_me'
+            // dbURI: 'mongodb://admineventarich:eventarich123@ds131711.mlab.com:31711/eventarich_me'
         }
 };
