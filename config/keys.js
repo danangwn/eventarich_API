b4a742eb8e1ef9d71a5c64aa1ef1c7fb1647f149
firgiawansaktyo <firgiawansaktyo@gmail.com>
2018-07-24 13:25:38 +0700

    lah kok error

--- config/keys.js
+++ config/keys.js
@@ -2,6 +2,5 @@ module.exports = {
         mongodb: {
             dbURI: 'mongodb://eventarich:eventarich11@ds131711.mlab.com:31711/eventarich_me'
             // dbURI: 'mongodb://admineventarich:eventarich123@ds131711.mlab.com:31711/eventarich_me'
-            // dbURI: 'mongodb://admineventarich:eventarich123@ds131711.mlab.com:31711/eventarich_me'
         }
 };
